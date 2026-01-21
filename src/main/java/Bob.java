045a760022cdc56efcc76e8ecc8952d40b82fd45
e1356231 <e1356231@u.nus.edu>
2026-01-22 06:00:53 +0800

    Level 1: Echo

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hi! I'm Bob");
         System.out.println("What can I help you with?");
-        System.out.println("Bye!");
+
+        while (true) {
+            System.out.println("Ask anything ");
+            String input = sc.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("Bye!");
+                break;
+            }
+            System.out.println("You asked: " + input);
+        }
+
     }
 }
