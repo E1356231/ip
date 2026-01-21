15dc8b9ac6827e2042b6cfbb67934fe3566692f8
e1356231 <e1356231@u.nus.edu>
2026-01-22 05:39:42 +0800

    Level 0: Rename, Greet, Exit

---
+++ src/main/java/Bob.java
@@ -0,0 +1,7 @@
+public class Bob {
+    public static void main(String[] args) {
+        System.out.println("Hi! I'm Bob");
+        System.out.println("What can I help you with?");
+        System.out.println("Bye!");
+    }
+}
