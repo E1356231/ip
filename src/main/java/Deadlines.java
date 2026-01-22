cdf0a13f59bc99dde2e68dd71501f577fe66a0ac
e1356231 <e1356231@u.nus.edu>
2026-01-22 15:11:18 +0800

    Level 4: ToDos, Deadlines, Events

---
+++ src/main/java/Deadlines.java
@@ -0,0 +1,15 @@
+public class Deadlines extends Task{
+    private String date;
+    private String time;
+
+    public Deadlines(String description, String date, String time) {
+        this.date = date;
+        this.time = time;
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[D] " + super.toString() + " by " + this.date + " " + this.time;
+    }
+}
---
+++ src/main/java/Events.java
@@ -0,0 +1,21 @@
+public class Events extends Task{
+    private String startDate;
+    private String startTime;
+    private String endDate;
+    private String endTime;
+
+    public Events(String description, String startDate, String startTime, String endDate, String endTime) {
+        this.startDate = startDate;
+        this.startTime = startTime;
+        this.endDate = endDate;
+        this.endTime = endTime;
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[E] " + super.toString() + " " + this.startDate + " to " + this.endDate + ": "
+                + this.startTime + " - " + this.endTime;
+    }
+
+}
