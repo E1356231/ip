4e7d66ac090a719d33426b95d47da07faa932d81
e1356231 <e1356231@u.nus.edu>
2026-01-22 11:36:04 +0800

    Level-2: Add, List

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -3,18 +3,28 @@ import java.util.Scanner;
 public class Bob {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = 1;
+        String[] tasks = new String[100];
         System.out.println("Hi! I'm Bob");
         System.out.println("What can I help you with?");
 
         while (true) {
-            System.out.println("Ask anything ");
+            System.out.println("Add task: ");
             String input = sc.nextLine();
+            tasks[n] = input;
 
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println("Bye!");
                 break;
             }
-            System.out.println("You asked: " + input);
+            else if (input.equalsIgnoreCase("tasks")) {
+                for (int i = 1; i < n; i++) {
+                    System.out.println(i + ". " + tasks[i]);
+                }
+                continue;
+            }
+            System.out.println("Task " + n + " added: " + input);
+            n++;
         }
 
     }
